c6a3ef2c29e0f179a05fb13cd6b520f674b0fe2d
Nicholas Yang <nick@nicholasyang.com>
2016-09-10 00:56:29 -0400

    added init file

---
+++ __init__.py
@@ -0,0 +1,24 @@
+import os
+from flask import (Flask,
+                   render_template,
+                   session,
+                   request,
+                   redirect,
+                   url_for,
+                   send_from_directory)
+
+
+app = Flask(__name__)
+
+flask_path = os.path.dirname(__file__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return send_from_directory('templates', 'ringtone.mp3')
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0")
