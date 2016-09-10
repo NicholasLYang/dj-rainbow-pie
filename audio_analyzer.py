baba74cf5bd1cbbf7992098929d9393da7fd0190
Nicholas Yang <nick@nicholasyang.com>
2016-09-10 18:04:17 -0400

    quick fix

--- audio_analyzer.py
+++ audio_analyzer.py
@@ -6,17 +6,18 @@ def read_mp3(file_name):
     duration = librosa.get_duration(filename = file_name)
     current_time = 0
     sample_size = 0.5
-
+    max = 0
     while current_time + sample_size < duration:
         waveform, sample_rate = librosa.load(path = file_name,
                                              offset = current_time,
                                              duration = sample_size)
-
         tuning = librosa.core.estimate_tuning(y = waveform,
                                               sr = sample_rate)
-        print (tuning + 0.5) * 255
+        if tuning > max:
+            max = tuning
+        scaled_tuning = ((tuning + 1) * 255) % 255
         current_time = current_time + sample_size
-
+    print max
 
 def scale_pitch(pitch):
     return color_wheel(pitch)
