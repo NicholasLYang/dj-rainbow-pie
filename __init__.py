ffd1aab6689ceb52505677b9b9e34ce9c27cd18e
Nicholas Yang <nick@nicholasyang.com>
2016-09-10 10:24:14 -0400

    minor bugfixes

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import os
+import utils
 from flask import (Flask,
                    render_template,
                    session,
@@ -6,18 +7,24 @@ from flask import (Flask,
                    redirect,
                    url_for,
                    send_from_directory)
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
 
 flask_path = os.path.dirname(__file__)
 
-@app.route("/")
-@app.route("/home")
+@app.route('/')
+@app.route('/home')
 def home():
     return send_from_directory('templates', 'ringtone.mp3')
 
+@app.route('/get_mp3/<file_name>')
+def get_mp3(file_name):
+    return send_from_directory('mp3s', file_name)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
